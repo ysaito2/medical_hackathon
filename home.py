d7db3e96dfc287d0d7c14a0bd79be12645f54fa9
@saito.yuta <saito.yuta@gridpredict.co.jp>
2023-09-23 18:10:43 +0900

    テーブルの実装

--- home.py
+++ home.py
@@ -2,23 +2,24 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+
 st.title('メディカルサーチ')
-region = st.text_input('地域：')
-symp = st.text_input('症状：')
-distance = st.text_input('移動距離：')
+hospital_name = st.text_input('病院名：')
+disease_name = st.text_input('病名：')
+symptom = st.text_input("症状: ")
 st.button(label='検索')
 
 # Excelファイルを読み込む
-df = pd.read_excel('/Users/yukitajima/Documents/file/Book1.xlsx')
+df = pd.read_excel('data/demo_data_v1.xlsx')
 
-# キーワードを指定
-keyword1 = region
-keyword2 = symp
+# 件数が大きい順番に並び替える
+filtered_df = df[(df["施設名"] == hospital_name) & (df["症状"] == disease_name)]
+sorted = filtered_df.sort_values(by="件数", ascending=False)
 
-# キーワードを含む行を抽出
-filtered_df = df[(df["区"] == keyword1) &(df["症状"] == keyword2)]
-print(filtered_df)
+# テーブルを表示
+st.table(sorted)
 
+# TODO: n件数のバープロットを表示
 # st.subheader('折れ線グラフ')
 # chart_data = pd.DataFrame(
 #      np.random.randn(7, 3),
@@ -27,3 +28,5 @@ print(filtered_df)
 # st.line_chart(chart_data)
 # st.dataframe(chart_data)
 
+# 地図を表示
+# st.map(sorted, "緯度", "経度", size="件数")
