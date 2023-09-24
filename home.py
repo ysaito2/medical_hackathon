6885e9f7b388c85d8c800909671608483fa8f869
@saito.yuta <saito.yuta@gridpredict.co.jp>
2023-09-24 13:29:55 +0900

    remove images

--- home.py
+++ home.py
@@ -4,17 +4,12 @@ import pandas as pd
 from streamlit_extras.switch_page_button import switch_page
 from PIL import Image
 
-image = Image.open('sunrise.jpg')
-
-st.image(image, caption='Sunrise by the mountains')
-
 
 st.title('メディカルサーチ')
 st.subheader("病院診断実績から良い病院を見つける")
-image = Image.open('images/front_page.png')
-st.image(image)
+# image = Image.open('images/front_page.png')
+# st.image(image)
 
-st.subheader("病院診断実績から良い病院を見つける")
 for_medical = st.button("患者様")
 for_patients = st.button("医療関係者様")
 
